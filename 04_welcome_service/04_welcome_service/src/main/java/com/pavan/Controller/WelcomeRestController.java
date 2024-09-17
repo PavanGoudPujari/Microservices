e6cdb87fe0e254ce8e7874996b7c91559e4314d1
Pavan <pavankumar.p9896@gmail.com>
2024-09-16 23:27:49 -0500

    added api gateway

--- 04_welcome_service/04_welcome_service/src/main/java/com/pavan/Controller/WelcomeRestController.java
+++ 04_welcome_service/04_welcome_service/src/main/java/com/pavan/Controller/WelcomeRestController.java
@@ -1,14 +1,21 @@
 package com.pavan.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/Welcome")
 	public String getWelcomeMsg() {
-		String str="Goodmorning pavan";
+		
+		String port = env.getProperty("server.port");
+		String str="Goodmorning pavan ("+ port +")";
 		return str;
 	}
 }
